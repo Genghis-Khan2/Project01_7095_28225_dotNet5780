a087e1a9e31c576f898d71ca33507197803c6b11
Genghis Khan <gkomet@g.jct.ac.il>
2019-12-05 13:12:48 +0200

    IDAL signatures

---
+++ DAL/IDAL.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    interface IDAL
+    {
+        public void addGuestRequest(Guest gr)
+        {
+
+        }
+
+        public void updateGuestRequest(Guest gr)
+        {
+
+        }
+
+
+        public void addHostingUnit(HostingUnit hu)
+        {
+
+        }
+
+        public void removeHostingUnit(HostingUnit hu)
+        {
+
+        }
+
+        public void updateHostingUnit(HostingUnit hu)
+        {
+
+        }
+
+
+        public void addOrder(Order ord)
+        {
+
+        }
+
+        public void updateOrder(Order ord)
+        {
+
+        }
+
+
+        public List<HostingUnit> getHostingUnits()
+        {
+
+        }
+
+        public List<Guest> getGuests()
+        {
+
+        }
+
+        public List<Order> getOrders()
+        {
+
+        }
+    }
+}
