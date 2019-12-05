04c504732a91f0bde157f017acb1c8ec453dc930
benshlom <benshlom@B104-52-86>
2019-12-05 13:20:49 +0200

    Creating the Enums.cs class, start work on GuestRequest.cs

---
+++ BE/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BE
+{
+    class Enums
+    {
+        public enum HostingUnitType { Zimmer, AccommodationApartment, HotelRoom, Encampment}
+        public enum Area { North, South, Center, Jerusalem }
+        public enum RequestStatus { UnTreated, SentMail, CustomerUnresponsiveness, CustomerResponsiveness }
+        public enum OrderStatus { open, ClosedWithDeal, CloseWithExpired}
+    }
+}
