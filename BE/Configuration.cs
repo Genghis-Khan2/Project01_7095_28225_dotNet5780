137cd037a7e6ec00b8052b3a361c398c2dc7e88d
benshlom <benshlom@B104-52-86>
2019-12-12 13:29:43 +0200

    :)

--- BE/Configuration.cs
+++ BE/Configuration.cs
@@ -7,7 +7,7 @@ namespace BE
     /// <summary>
     /// The class contains all the static variables required for the software classes
     /// </summary>
-    class Configuration
+    public class Configuration
     {
         public static int GuestRequestKey = 0;//using by GuestRequest and Order
         public static int BankNumber = 0;//using by BankAccount
