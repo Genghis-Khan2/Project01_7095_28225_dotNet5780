c96d6e3f16cb2da0add0ad966f6eee4cd9fd5a38
Genghis Khan <gkomet@g.jct.ac.il>
2019-12-19 12:43:21 +0200

    Regioned IDAL and DalImp

--- DAL/DalImp.cs
+++ DAL/DalImp.cs
@@ -9,6 +9,7 @@ namespace DAL
 {
     public class DalImp : IDAL
     {
+        #region Singletory These parts are what make the class a singletory
         private DalImp() { }
 
         protected static DalImp instance = null;
@@ -25,7 +26,10 @@ namespace DAL
             }
             return instance;
         }
+        #endregion
 
+        #region GuestRequest These functions perform actions on GuestRequests
+        #region AddGuestRequest This function adds a guest request
         /// <summary>
         /// This function adds a guest request to the data's list
         /// </summary>
@@ -41,7 +45,36 @@ namespace DAL
                 DataSource.guestRequestsList.Add(gr.Clone());
             }
         }
+        #endregion
 
+        #region GetAllGuestRequests This function returns the guest requests
+        /// <summary>
+        /// This function returns the guest requests in the data
+        /// </summary>
+        /// <returns>IEnumerable to go over the list of guest requests</returns>
+        public IEnumerable<GuestRequest> GetAllGuestRequests()
+        {
+            return from item in DataSource.guestRequestsList.Clone()
+                   select item;
+        }
+        #endregion
+
+        #region UpdateGuestRequest This function updates a guest request
+        /// <summary>
+        /// This function updates a guest request
+        /// </summary>
+        /// <param name="gr">Guest request to update to</param>
+        /// <param name="key">Key of guest request to update</param>
+        public void UpdateGuestRequest(GuestRequest gr, int key)
+        {
+            int i = DataSource.guestRequestsList.FindIndex(t => t.GuestRequestKey == key);
+            DataSource.guestRequestsList[i] = gr;
+        }
+        #endregion
+        #endregion
+
+        #region HostingUnit These functions perform actions on HostingUnits
+        #region AddHostingUnit This function adds a hosting unit
         /// <summary>
         /// This function adds a hosting unit to the data's list
         /// </summary>
@@ -57,7 +90,56 @@ namespace DAL
                 DataSource.hostingUnitsList.Add(hu.Clone());
             }
         }
+        #endregion
 
+        #region GetAllHostingUnits This function returns all hosting units
+        /// <summary>
+        /// This function returns the hosting units in the data
+        /// </summary>
+        /// <returns>IEnumerable to go over the list of hosting units</returns>
+        public IEnumerable<HostingUnit> GetAllHostingUnits()
+        {
+            return from item in DataSource.hostingUnitsList.Clone()
+                   select item;
+        }
+        #endregion
+
+        #region RemoveHostingUnit This function removes a hosting unit
+        /// <summary>
+        /// This function removes a hosting unit from the data
+        /// </summary>
+        /// Important Note: It will not compare all fields. It will only compare the key 
+        /// <param name="key">Key to remove the hosting unit of</param>
+        public void RemoveHostingUnit(int key)
+        {
+            var res = from item in DataSource.hostingUnitsList
+                      let temp = key
+                      where temp == item.HostingUnitKey
+                      select item;
+
+            foreach (var it in res)
+            {
+                DataSource.hostingUnitsList.Remove(it);
+            }
+        }
+        #endregion
+
+        #region UpdateHostingUnit This function updates a hosting unit
+        /// <summary>
+        /// This function updates a hosting unit
+        /// </summary>
+        /// <param name="hu">Hosting unit to update to</param>
+        /// <param name="key">Key of hosting unit to update</param>
+        public void UpdateHostingUnit(HostingUnit hu, int key)
+        {
+            int index = DataSource.hostingUnitsList.FindIndex(new Predicate<HostingUnit>(x => x.HostingUnitKey == key));
+            DataSource.hostingUnitsList[index] = hu.Clone();
+        }
+        #endregion
+        #endregion
+
+        #region Order These functions perform actions on Orders
+        #region AddOrder This function adds and order
         /// <summary>
         /// This function addes an order to the data's list
         /// </summary>
@@ -73,7 +155,35 @@ namespace DAL
                 DataSource.ordersList.Add(ord.Clone());
             }
         }
+        #endregion
 
+        #region GetAllOrders This function returns all the orders
+        /// <summary>
+        /// This function returns the orders in the data
+        /// </summary>
+        /// <returns>IEnumerable to go over the list of orders</returns>
+        public IEnumerable<Order> GetAllOrders()
+        {
+            return from item in DataSource.ordersList.Clone()
+                   select item;
+        }
+        #endregion
+
+        #region UpdateOrder This function updates an order
+        /// <summary>
+        /// This function updates an order
+        /// </summary>
+        /// <param name="ord">Order to update to</param>
+        /// <param name="key">Key of order to update</param>
+        public void UpdateOrder(Order ord, int key)
+        {
+            int index = DataSource.ordersList.FindIndex(new Predicate<Order>(x => x.OrderKey == key));
+            DataSource.ordersList[index] = ord.Clone();
+        }
+        #endregion
+        #endregion
+
+        #region GetAllBankAccounts This function returns all the bank accounts
         /// <summary>
         /// This function returns the bank accounts in the data
         /// </summary>
@@ -130,86 +240,6 @@ namespace DAL
             return from item in ret.Clone()
                    select item;
         }
-
-        /// <summary>
-        /// This function returns the guest requests in the data
-        /// </summary>
-        /// <returns>IEnumerable to go over the list of guest requests</returns>
-        public IEnumerable<GuestRequest> GetAllGuestRequests()
-        {
-            return from item in DataSource.guestRequestsList.Clone()
-                   select item;
-        }
-
-        /// <summary>
-        /// This function returns the hosting units in the data
-        /// </summary>
-        /// <returns>IEnumerable to go over the list of hosting units</returns>
-        public IEnumerable<HostingUnit> GetAllHostingUnits()
-        {
-            return from item in DataSource.hostingUnitsList.Clone()
-                   select item;
-        }
-
-        /// <summary>
-        /// This function returns the orders in the data
-        /// </summary>
-        /// <returns>IEnumerable to go over the list of orders</returns>
-        public IEnumerable<Order> GetAllOrders()
-        {
-            return from item in DataSource.ordersList.Clone()
-                   select item;
-        }
-
-        /// <summary>
-        /// This function removes a hosting unit from the data
-        /// </summary>
-        /// Important Note: It will not compare all fields. It will only compare the key 
-        /// <param name="key">Key to remove the hosting unit of</param>
-        public void RemoveHostingUnit(int key)
-        {
-            var res = from item in DataSource.hostingUnitsList
-                      let temp = key
-                      where temp == item.HostingUnitKey
-                      select item;
-
-            foreach (var it in res)
-            {
-                DataSource.hostingUnitsList.Remove(it);
-            }
-        }
-
-        /// <summary>
-        /// This function updates a guest request
-        /// </summary>
-        /// <param name="gr">Guest request to update to</param>
-        /// <param name="key">Key of guest request to update</param>
-        public void UpdateGuestRequest(GuestRequest gr, int key)
-        {
-            int i = DataSource.guestRequestsList.FindIndex(t => t.GuestRequestKey == key);
-            DataSource.guestRequestsList[i] = gr;
-        }
-
-        /// <summary>
-        /// This function updates a hosting unit
-        /// </summary>
-        /// <param name="hu">Hosting unit to update to</param>
-        /// <param name="key">Key of hosting unit to update</param>
-        public void UpdateHostingUnit(HostingUnit hu, int key)
-        {
-            int index = DataSource.hostingUnitsList.FindIndex(new Predicate<HostingUnit>(x => x.HostingUnitKey == key));
-            DataSource.hostingUnitsList[index] = hu.Clone();
-        }
-
-        /// <summary>
-        /// This function updates an order
-        /// </summary>
-        /// <param name="ord">Order to update to</param>
-        /// <param name="key">Key of order to update</param>
-        public void UpdateOrder(Order ord, int key)
-        {
-            int index = DataSource.ordersList.FindIndex(new Predicate<Order>(x => x.OrderKey == key));
-            DataSource.ordersList[index] = ord.Clone();
-        }
+        #endregion
     }
 }
