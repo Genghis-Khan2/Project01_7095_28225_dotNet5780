0afa07cc30580df372b512daac52373e4c674214
Genghis Khan <gkomet@g.jct.ac.il>
2019-12-19 17:44:37 +0200

    In the middle of adding a lot of documentation

--- Exceptions/AlreadyExistsException.cs
+++ Exceptions/AlreadyExistsException.cs
@@ -4,9 +4,22 @@ using System.Text;
 
 namespace Exceptions
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class AlreadyExistsException : Exception
     {
-
+        /// <summary>
+        /// Constructor that creates an exception with a specific error message suited
+        /// to the key that exists(<paramref name="val"/>) and type stored in the list(<paramref name="type"/>)
+        /// </summary>
+        /// <param name="val">Key that already exists</param>
+        /// <param name="type">Data type stored in the list</param>
+        /// <example>
+        /// <code>
+        /// 
+        /// </code>
+        /// </example>
         public AlreadyExistsException(int val, string type)
             : base(String.Format("The key {0} already exists as a {1}", val, type))
         {
--- Exceptions/NoItemsException.cs
+++ Exceptions/NoItemsException.cs
@@ -6,12 +6,23 @@ namespace Exceptions
 {
     public class NoItemsException : Exception
     {
+        /// <summary>
+        /// Standard constructor
+        /// </summary>
+        /// <remarks>
+        /// Message is non-specific to a certain type
+        /// </remarks>
+        /// See <see cref="NoItemsException(string)"/> for a specific exception
         public NoItemsException()
             : base("No items exist")
         {
 
         }
 
+        /// <summary>
+        /// Constructor that creates a specific error message
+        /// </summary>
+        /// <param name="message">Data type stored in the list</param>
         public NoItemsException(string message)
             : base(String.Format("No items exist in the {0} list", message))
         {
