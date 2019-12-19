40169780f3e767d35aa2fb76f57f3f285ad9bc3a
benshlom <benshlom@B104-52-86>
2019-12-19 12:00:06 +0200

    "

--- BL/BLImp.cs
+++ BL/BLImp.cs
@@ -98,7 +98,7 @@ namespace BL
             //TODO: write the function
             throw new NotImplementedException();
         }
-        public int getNumberOfDateInRange(DateTime startDay)//check 2 param
+        public int getNumberOfDateInRange(DateTime startDay, )//check 2 param
         {
             //remember to reset the year of end Day to 2020 - maibe dont need because we dont use it?
             //TODO: write the function
@@ -114,6 +114,25 @@ namespace BL
             //TODO: write the function
             throw new NotImplementedException();
         }
-
+        /*
+         
+ פונקציה שמקבלת תאריך אחד או שניים. הפונקציה מחזירה את מספר הימים שעברו
+מהתאריך הראשון ועד לשני, או במיה והתקבל רק תאריך אחד – מהתאריך ראשון ועד
+היום.
+ פונקציה שמקבלת מספר ימים, ומחזירה את כל ההזמנות שמשך הזמן שעבר מאז שנוצרו /
+מאז שנשלח מייל ללקוח גדול או שווה למספר הימים שהפונקציה קיבלה. 
+בס"ד פרויקט איתור והתאמת נופש, דוט נט התש"פ
+17
+ פונקציה שיכולה להחזיר את כל הלקוחות שמתאימים לתנאי מסוים )הכוונה שהפונקציה
+מקבלת delegate שמתאים למתודה שפועלת על לקוח ומחזירה bool וכך מוגדר התנאי(
+ פונקציה שמקבלת לקוח, ומחזירה את מספר ההזמנות שנשלחו אליו.
+ פונקציה שמקבלת יחידת אירוח ומחזירה את מספר ההזמנות שנשלחו / מספר ההזמנות
+שנסגרו בהצלחה עבור יחידה זו דרך האתר.
+הגדר פונקציות המחזירות את הקבוצות הבאות )ע"י שימוש ב-Grouping )
+ רשימת לקוחות מקובצת )Grouping )ע"פ אזור הנופש הנדרש.
+ רשימת לקוחות מקובצת )Grouping )ע"פ מספר הנופשים.
+ רשימת מארחים מקובצת )Grouping )ע"פ מספר יחידות האירוח שהם מחזיקים
+ רשימת יחידות אירוח מקובצת )Grouping )ע"פ אזור הנופש הנדרש.
+         */
     }
 }
