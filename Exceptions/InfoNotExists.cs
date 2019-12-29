0f09c9a57d92ee12d7a3f7b3668f319440f97a36
Genghis Khan <gkomet@g.jct.ac.il>
2019-12-29 10:51:25 +0200

    Added InfoNotExists Exception

---
+++ Exceptions/InfoNotExists.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exceptions
+{
+    public class InfoNotExists : Exception
+    {
+        /// <summary>
+        /// Constructor that creates an exception with a specific error message suited
+        /// to the type who's info does not exist (<paramref name="typeNotExisting"/>), and which type tried to get the info that deos not exist (<paramref name="typeCheckingExisting"/>)
+        /// </summary>
+        /// <param name="typeNotExisting">The type who's info does not exist in the database</param>
+        /// <param name="typeCheckingExisting">The type that needs the info that does not exist</param>
+        /// <example>
+        /// <code>
+        /// var e = new InfoNotExists("GuestRequest", "Order");
+        /// Console.WriteLine(e.Message);
+        /// "The GuestRequest does not exist for Order"
+        /// </code>
+        /// </example>
+        public InfoNotExists(string typeNotExisting, string typeCheckingExisting)
+            : base(String.Format("The {0} does not exist for {1}", typeNotExisting, typeCheckingExisting))
+        {
+
+        }
+    }
+}
