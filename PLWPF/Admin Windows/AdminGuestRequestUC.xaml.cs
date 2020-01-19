895053f737916307837220f2f68a1029bc73a2cb
Genghis Khan <gkomet@g.jct.ac.il>
2020-01-19 10:19:07 +0200

    Class

---
+++ PLWPF/Admin Windows/AdminGuestRequestUC.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PLWPF.Admin_Windows
+{
+    /// <summary>
+    /// Interaction logic for AdminGuestRequestUC.xaml
+    /// </summary>
+    public partial class AdminGuestRequestUC : UserControl
+    {
+        public AdminGuestRequestUC(BE.GuestRequest guestRequest)
+        {
+            InitializeComponent();
+            Name.Content = guestRequest.PrivateName + " " + guestRequest.FamilyName;
+            Duration.Content = string.Format("{0}.{2} - {1}.{3}", guestRequest.EntryDate.Day, guestRequest.ReleaseDate.Day,
+                guestRequest.EntryDate.Month, guestRequest.ReleaseDate.Month);
+            MailAddress.Content = guestRequest.MailAddress;
+        }
+    }
+}
