ff3bb795fc23f9376032c196374c670107a21e7b
Genghis Khan <gkomet@g.jct.ac.il>
2020-01-27 15:22:26 +0200

    Adding docs for XML_IMP

--- PLWPF/Wizard/WizardContainer.xaml.cs
+++ PLWPF/Wizard/WizardContainer.xaml.cs
@@ -10,19 +10,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace PLWPF
+namespace PLWPF.Wizard
 {
     /// <summary>
     /// Interaction logic for WizardContainer.xaml
     /// </summary>
     public partial class WizardContainer : Window
     {
-
         public WizardContainer()
         {
             InitializeComponent();
         }
-
-
     }
 }
